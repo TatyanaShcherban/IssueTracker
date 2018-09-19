e9d451ae2cea818f59ccdc3bb00581fd2616ba4b
TatyanaShcherban <43166990+TatyanaShcherban@users.noreply.github.com>
2018-09-19 12:07:23 +0300

    The AddToDataBaseNewIssue(Issue issue) method was improved
    The AddToDataBaseNewIssue(Issue issue) method was improved. Some methods were changed.

--- IssueTracker/Controllers/HomeController.cs
+++ IssueTracker/Controllers/HomeController.cs
@@ -5,50 +5,69 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//for me: improve code: exceptions, dublicate 
+
 namespace IssueTracker.Controllers
 {
     public class HomeController : Controller
     {
-        // create data context
+        // Create data context
         Context dataBaseObject = new Context();
+        // Main view
         public ActionResult Index()
         {
             SelectList allProjects = new SelectList(dataBaseObject.Projects, "id", "Name");
             ViewBag.Projects = allProjects;
-            IEnumerable<Issue> allIssues = dataBaseObject.Issues;
+            IEnumerable<Issue> allIssues = dataBaseObject.Issues.Where(item => item.ProjectID == 4);
             ViewBag.Issues = allIssues;
             return View();
         }
 
+        // This method for changing tables 
+        [HttpGet]
+        public ActionResult GetIssueData(int id)
+        {
+            IEnumerable<Issue> allIssues = dataBaseObject.Issues.Where(item => item.ProjectID == id);
+            ViewBag.Issues = allIssues;
+            return PartialView("~/Views/Home/_GetIssueDataPartial.cshtml", ViewBag.Issues);
+        }
+        // This method for opening modal window to create new issue 
         [HttpGet]
         public ActionResult CreateIssueWindow(int id)
         {
-            IEnumerable<Project> choosedProject = dataBaseObject.Projects.Where(item => item.Id == id);
-            ViewBag.Projects = choosedProject;
-            return PartialView("~/Views/Home/NewIssueWindow.cshtml", ViewBag.Projects);
+            Project choosedProject = dataBaseObject.Projects.FirstOrDefault(item => item.Id == id);
+            if (choosedProject != null)
+                return PartialView("~/Views/Home/NewIssueWindow.cshtml", choosedProject);
+            return HttpNotFound();
         }
 
         [HttpGet]
         public ActionResult EditIssueWindow(int id)
         {
-            IEnumerable<Issue> allIssues = dataBaseObject.Issues.Where(item => item.ProjectID == id);
-            ViewBag.Issues = allIssues;
-            return PartialView("~/Views/Home/NewIssueWindow.cshtml");
+            Project choosedProject = dataBaseObject.Projects.Find(id);
+            ViewBag.Projects = dataBaseObject.Projects.ToList();
+            //Project choosedProject = dataBaseObject.Projects.FirstOrDefault(item => item.Id == id);
+            //Issue editedIssue = dataBaseObject.Projects.FirstOrDefault(item => item.);
+            if (choosedProject != null)
+                return PartialView("~/Views/Home/NewIssueWindow.cshtml", choosedProject);
+            return HttpNotFound();
         }
 
+        // Saving to database 
         [HttpPost]
-        public string AddToDataBaseNewIssue(Issue issue)
+        public ActionResult AddToDataBaseNewIssue(Issue issue)
         {
             try
             {
                 dataBaseObject.Issues.Add(issue);
                 dataBaseObject.SaveChanges();
             }
-            catch (Exception ex){
-                return ex.ToString();
+            catch (Exception ex)
+            {  //сузить ошибку
+                HttpNotFound();// вывести нужную ошибку                                        !!!
             }
-            return "Saved";
-        }
+            return RedirectToAction("Index");
 
+        }
     }
 }
