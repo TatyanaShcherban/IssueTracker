5a032a8f261d24b61c04349f42299367acd84228
TatyanaShcherban <43166990+TatyanaShcherban@users.noreply.github.com>
2018-10-02 17:46:17 +0300

    Include Site.css file
    Include Site.css file

--- IssueTracker/App_Start/BundleConfig.cs
+++ IssueTracker/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace IssueTracker
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
